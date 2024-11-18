6b2e36622da64e66f5a81455dc9621591e5a4ab7
hoanhua14 <huasohoan@gmail.com>
2024-11-17 17:06:23 -0800

    tested mockdata

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,3 +1,4 @@
+import mockData from "./data";
 const Dashboard = () => {
   return <div>blah</div>;
 };
--- src/data.js
+++ src/data.js
@@ -1,7 +1,17 @@
 let mockData = [
   {
     address: "298",
-    customer: "Thanh",
+
     unit: "1",
+    customer: "Thanh",
+    rent: "1000",
+    prevElec: "12",
+    currElec: "14",
+    billElec: "20202",
+    prevWater: "33",
+    currWater: "35",
+    billWater: "3993",
+    total: "30303030",
   },
 ];
+export default mockData;
