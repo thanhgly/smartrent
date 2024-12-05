79c7803fd023a961f60523de1b8d3f2ddc0b0826
Thanh Ly <thanhgly.se@gmail.com>
2024-12-04 16:33:35 -0800

    set up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Specify the development server port
+    port: 3001,
+  },
+  // Base name of your app
+  base: "/", // Replace this with the subdirectory path if needed
+});
